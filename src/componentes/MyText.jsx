f274184aad7dc0a23ae562c482e692a5e82c2396
julianasoares77 <julianaasoares77@gmail.com>
2025-05-22 19:20:35 +0000

    Aula 22/05/2025-criação de mytext

---
+++ src/componentes/MyText.jsx
@@ -0,0 +1,14 @@
+import styles from './MyText.module.css'
+
+export function MyText() {
+    return (
+        <div className={styles.container}>
+        <div className={styles.div}> 
+            <h1 className={styles.title}>Meu primeiro React App</h1>
+            <p className={styles.Text}> 
+           JULIANAA
+            </p>
+        </div>
+        </div>
+    );
+}
